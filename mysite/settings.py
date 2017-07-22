79526e5bd0c48af3af2f5c644c5e61ca296878da
lacgoal <lac.goal@hotmail.com>
2017-07-22 13:15:31 -0500

    fixed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8sb+f$ay76wne-ui2gz%g-bolj78z)enc92@j%xdf%5o!(jrqb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'lacgoal.pythonanywhere.com']
 
 
 # Application definition
